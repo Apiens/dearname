e37f8402af73c5ae02c076d7962089186a2c3f14
Jaeho Lee <insect_coka@naver.com>
2021-02-05 19:47:14 +0900

    add api_host info

--- frontend/src/Constants.tsx
+++ frontend/src/Constants.tsx
@@ -1,4 +1,4 @@
 export const API_HOST =
-  process.env.REACT_APP_API_HOST || "Http://localhost:8000";
+  process.env.REACT_APP_API_HOST || "https://ddec3rgmn9iic.cloudfront.net/";
 
 export const PREDICT_API_HOST = "http://localhost:8008";
