cbd7c7f91dcec4afae63a0360e5636bf557fc7ed
Jaeho Lee <insect_coka@naver.com>
2021-02-10 22:22:30 +0900

    add public_url and predict_api_host in FE

--- frontend/src/Constants.tsx
+++ frontend/src/Constants.tsx
@@ -1,4 +1,5 @@
 export const API_HOST =
   process.env.REACT_APP_API_HOST || "https://ddec3rgmn9iic.cloudfront.net/";
 
-export const PREDICT_API_HOST = "http://localhost:8008";
+export const PREDICT_API_HOST =
+  process.env.PREDICT_API_HOST || "https://ddec3rgmn9iic.cloudfront.net/";
