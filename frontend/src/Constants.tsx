ae94aed4ca7d8e15996c279b01f3a020b0b73795
Jaeho Lee <insect_coka@naver.com>
2021-02-11 21:34:29 +0900

    update react_app_api_host

--- frontend/src/Constants.tsx
+++ frontend/src/Constants.tsx
@@ -1,5 +1,9 @@
 export const API_HOST =
-  process.env.REACT_APP_API_HOST || "https://ddec3rgmn9iic.cloudfront.net/";
+  process.env.REACT_APP_API_HOST ||
+  "https://d16c239m5uwjv8.cloudfront.net/" ||
+  "https://ddec3rgmn9iic.cloudfront.net/";
 
 export const PREDICT_API_HOST =
-  process.env.PREDICT_API_HOST || "https://ddec3rgmn9iic.cloudfront.net/";
+  process.env.PREDICT_API_HOST ||
+  "https://d16c239m5uwjv8.cloudfront.net/" ||
+  "https://ddec3rgmn9iic.cloudfront.net/";
